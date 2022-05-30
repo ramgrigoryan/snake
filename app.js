5efdfdafb20668bef0ac783d8ebdbd8ab590d298
Vahram <ramgrigoryan@gmail.com>
2022-05-30 20:31:22 +0400

    teleport

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ document.addEventListener("keydown", (ev) => {
     case LEFT:
       if (snake.offsetLeft > 0) {
         snake.style.left = `${snake.offsetLeft - 10}px`;
+      } else {
+        snake.style.left = "480px";
       }
       break;
     case RIGHT:
       if (snake.offsetLeft < 480) {
         snake.style.left = `${snake.offsetLeft + 10}px`;
+      } else {
+        snake.style.left = "0px";
       }
       break;
     case UP:
       if (snake.offsetTop > 0) {
         snake.style.top = `${snake.offsetTop - 10}px`;
+      } else {
+        snake.style.top = "480px";
       }
       break;
     case DOWN:
       if (snake.offsetTop < 480) {
         snake.style.top = `${snake.offsetTop + 10}px`;
+      } else {
+        snake.style.top = "0px";
       }
       break;
     default:
