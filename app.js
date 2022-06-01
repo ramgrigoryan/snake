5be320aef94ae1332e9ded84dc2ad93af5cdc354
= <=>
2022-05-31 19:01:06 -0700

    food generated

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const RIGHT = "ArrowRight";
 const UP = "ArrowUp";
 const DOWN = "ArrowDown";
 const snake = document.querySelector(".snake");
+const canvas = document.getElementById("canvas");
+const randomCoord = (from, to) => {
+  let a = Math.floor((to - from) * Math.random())
+   return a - a%10;
+}
+const food = snake.cloneNode(true);
+food.style.top = `${randomCoord(0, 480)}px`;
+food.style.left = `${randomCoord(0, 480)}px`;
+canvas.append(food);
 document.addEventListener("keydown", (ev) => {
   switch (ev.code) {
     case LEFT:
@@ -36,5 +45,4 @@ document.addEventListener("keydown", (ev) => {
     default:
       return;
   }
-  console.log(snake.offsetLeft, snake.offsetTop);
 });
